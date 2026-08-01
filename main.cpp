1b1c6e69af77063fd6f0e901a9bbed217d6cd39c
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-01 09:56:06 +0200

    Update main.cpp
    passe en blanc

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <QApplication>
+#include <QPalette>
 #include "mainwindow.h"
 #include <QtPlugin>
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
+
+  QPalette palette = a.palette();
+  palette.setColor(QPalette::Window, Qt::white);
+  palette.setColor(QPalette::Base, Qt::white);
+  a.setPalette(palette);
+
   MainWindow w;
 
   w.show();
