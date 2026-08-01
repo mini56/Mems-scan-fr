dc3974d485f125b9e4b588e325104a6158195e30
mini56 <48644212+mini56@users.noreply.github.com>
2026-08-01 10:07:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,27 @@ int main(int argc, char *argv[])
   palette.setColor(QPalette::Base, Qt::white);
   a.setPalette(palette);
 
+  a.setStyleSheet(
+    "QPushButton {"
+    "  background-color: #2f6fed;"
+    "  color: white;"
+    "  border: none;"
+    "  border-radius: 6px;"
+    "  padding: 6px 14px;"
+    "  font-weight: 600;"
+    "}"
+    "QPushButton:hover {"
+    "  background-color: #245bd1;"
+    "}"
+    "QPushButton:pressed {"
+    "  background-color: #1c48a8;"
+    "}"
+    "QPushButton:disabled {"
+    "  background-color: #c7cbd4;"
+    "  color: #8b909c;"
+    "}"
+  );
+
   MainWindow w;
 
   w.show();
